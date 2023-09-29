640c7212cd2015827c0f9f05fddfcb14314a1fe5
Gaspare <Gas9220@users.noreply.github.com>
2023-09-29 12:53:53 +0200

    PizzaController

---
+++ la-mia-pizzeria-crud-mvc/Controllers/PizzaController.cs
@@ -0,0 +1,37 @@
+﻿using la_mia_pizzeria_crud_mvc.Database;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_crud_mvc.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizzas = db.Pizzas.ToList<Pizza>();
+
+                return View("Index", pizzas);
+            }
+
+        }
+
+        public IActionResult Details(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza? foundedPizza = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (foundedPizza == null)
+                {
+                    return NotFound($"Nessuna pizza trovata con l'id {id} ");
+                }
+                else
+                {
+                    return View("Details", foundedPizza);
+                }
+            }
+        }
+    }
+}
