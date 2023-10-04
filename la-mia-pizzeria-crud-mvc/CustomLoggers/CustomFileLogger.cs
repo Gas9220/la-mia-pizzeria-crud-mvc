8037d16571b2c527e24da75663c527c1aade7339
Gaspare <gasmo92@gmail.com>
2023-10-04 09:14:15 +0200

    fix CustomFileLogger

--- la-mia-pizzeria-crud-mvc/CustomLoggers/CustomFileLogger.cs
+++ la-mia-pizzeria-crud-mvc/CustomLoggers/CustomFileLogger.cs
@@ -5,7 +5,7 @@
         public void WriteLog(string message, string operation)
         {
             // projectFolder/bin/net7.0/log.txt
-            File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "\\log.txt", $"{DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")} LOG: {message} OPERATION: {operation}\n");
+            File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "\\log.txt", $"{DateTime.Now:dd-MM-yyyy HH:mm:ss} LOG: {message} OPERATION: {operation}\n");
         }
     }
 }
