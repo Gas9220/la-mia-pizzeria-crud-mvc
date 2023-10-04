2b953923be5d66cefd453b446b4308b76abfe890
Gaspare <gasmo92@gmail.com>
2023-10-04 11:44:59 +0200

    update PizzaContext with Category

--- la-mia-pizzeria-crud-mvc/Database/PizzaContext.cs
+++ la-mia-pizzeria-crud-mvc/Database/PizzaContext.cs
@@ -6,6 +6,7 @@ namespace la_mia_pizzeria_crud_mvc.Database
     public class PizzaContext: DbContext
     {
         public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Category> Categories { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PizzaDb;" +
