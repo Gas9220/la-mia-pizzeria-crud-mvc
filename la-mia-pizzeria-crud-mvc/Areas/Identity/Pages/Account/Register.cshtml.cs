adc335e432fb63951bb6bf420840fef287ce7188
Gaspare <gasmo92@gmail.com>
2023-10-06 14:20:30 +0200

    Default user role on registration

--- la-mia-pizzeria-crud-mvc/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ la-mia-pizzeria-crud-mvc/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -113,7 +113,7 @@ namespace la_mia_pizzeria_crud_mvc.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-
+                await _userManager.AddToRoleAsync(user, "USER");
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
